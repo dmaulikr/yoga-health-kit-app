912f0b9f030d8b5321884f7f759c81ca7b0fde0c
Kat Drobnjakovic <katdrobnjakovic@kats-macbook-pro-2.local>
2016-03-03 14:06:23 -0500

    Initial Commit

---
+++ yoga-health-kit-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yoga-health-kit-app
+//
+//  Created by Kat Drobnjakovic on 2016-03-03.
+//  Copyright © 2016 Kat Drobnjakovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
