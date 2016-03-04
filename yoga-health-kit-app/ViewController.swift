d98a65a528828826025ae48290babf939142d9a2
Kat Drobnjakovic <katdrobnjakovic@Kats-MacBook-Pro-2.local>
2016-03-03 21:39:03 -0500

    enter and leave pure

--- yoga-health-kit-app/ViewController.swift
+++ yoga-health-kit-app/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let manager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+        
+        let coordinates = CLLocationCoordinate2D(latitude: 45.4143192, longitude: -75.6973991)
+        let region = CLCircularRegion(center: coordinates, radius: 20.0, identifier: "Pure Yoga")
+        manager.startMonitoringForRegion(region)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    internal func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        print(status)
+    }
+        
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("entered pure yoga")
+    }
+    
+    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("exit pure yoga")
+    }
 
 }
 
